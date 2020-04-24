a26bb3db5924c7d80398a18340f7ebe63442e1fc
Fumitoshi Morisato <fumitoshi.morisato@access-company.com>
2020-04-24 09:12:30 +0900

    modify import

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import { memberAddButton, memberNamesList, shuffleButton, roomNumInput, venue } from "./src/js/DI/dom.js"
 import addMemberField from "./src/js/presenter/addMemberField.js"
 import collectMembers from "./src/js/controller/collectMembersFromFields.js"
+import groupMembers from "./src/js/controller/groupingMembers.js"
+import shuffleMembers from "./src/js/controller/shuffleMembers.js"
 import cleanUpVenue from "./src/js/presenter/cleanUpVenue.js"
 import moveIntoRooms from "./src/js/presenter/moveIntoRooms.js"
 import shuffle from "./src/js/util/naiveShuffle.js"
@@ -22,7 +24,7 @@ function main() {
             const members = collectMembers(memberNamesList.children)
             const shuffled = shuffleMembers(shuffle)(members)
             const roomNum = parseInt(roomNumInput.value)
-            const groups = grouping(roomNum)(shuffled)
+            const groups = groupMembers(roomNum)(shuffled)
             cleanUpVenue(venue)
             moveIntoRooms(venue)(groups)
         }
